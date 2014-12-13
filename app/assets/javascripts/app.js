377e75160e674d45e523beb33af4085414c5e21e
Evan <evturn@gmail.com>
2014-12-13 16:38:53 -0500

    building namespace in app

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,1 +1,18 @@
 console.log('we got app');
+
+var app = (function(){
+
+	var api = {
+		views			 : {},
+		models		 : {},
+		collections: {},
+		content		 : null,
+		router		 : null,
+		todos			 : null,
+		init			 : function() {
+			this.content = $("content");
+		},
+
+	}
+
+});
