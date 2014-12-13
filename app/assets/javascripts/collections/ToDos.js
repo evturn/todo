a8d942a1e2d842df1563f5088e0bca5160c2b9b0
Evan <evturn@gmail.com>
2014-12-13 18:12:00 -0500

    collection created for models

--- app/assets/javascripts/collections/ToDos.js
+++ app/assets/javascripts/collections/ToDos.js
@@ -1,1 +1,27 @@
 console.log('we got ToDo Collection');
+
+app.collections.ToDos = Backbone.Collection.extend({
+	initialize: function(){
+		this.add({ title: "Take notes" });
+		this.add({ title: "Code along" });
+		this.add({ title: "Try to make sense of this" });
+	},
+	model: app.models.ToDo
+	up: function(index){
+		if(index > o) {
+			var tmp = this.models[index-1];
+			this.models[index] = tmp;
+			this.trigger("change");
+		}
+	},
+	down: function(index) {
+		if(index < this.models.length-1) {
+			var tmp = this.models[index+1];
+			this.models[index] = tmp;
+			this.trigger("change");
+		}
+	},
+	archive: function(archived, index){
+		this.models[index].set("done", done);
+	}
+});
--- app/assets/javascripts/models/ToDo.js
+++ app/assets/javascripts/models/ToDo.js
@@ -1,1 +1,9 @@
 console.log('we got ToDo Model');
+
+app.models.ToDo = Backbone.Model.extend({
+		defaults: {
+			title: "ToDo",
+			archived: false,
+			done: false
+		}
+});
