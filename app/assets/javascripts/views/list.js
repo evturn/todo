031f2b8172b8dec9baa1d20d7e5fccd223740696
Evan <evturn@gmail.com>
2014-12-13 21:13:59 -0500

    render function for list built

--- app/assets/javascripts/views/list.js
+++ app/assets/javascripts/views/list.js
@@ -7,7 +7,26 @@ app.views.list = Backbone.View.extend({
 			this.model.bind('add', handler);
 			this.model.bind('remove', handler);
 		},
-		render: function() {},
+		render: function() {
+			var html = '<ul class="list>',
+					self = this;
+			this.model.each(function(todo, index) {
+				if(self.mode === "archive" ? todo.get("archived") === true : todo.get("archived") === false) {
+						var template = _.template($("#tpl-list-item").html());
+						html += template({
+							title: todo.get("title"),
+							index: index,
+							archivedLink: self.mode === "archive" ? "unarchive" : "archive",
+							done: todo.get("done") ? "yes" : "no",
+							doneChecked: todo.get("done") ? 'checked="checked"' : ""
+						})
+						html =+ '</ul>';
+						this.$el.html(html);
+						this.delegateEvents();
+						return this;
+				}
+			})
+		},
 		priorityUp: function(e) {},
 		priorityDown: function(e) {},
 		archive: function(e) {},
